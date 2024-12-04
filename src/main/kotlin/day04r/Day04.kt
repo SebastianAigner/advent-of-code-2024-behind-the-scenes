4d1729fecd8098de1f6bac0dc4b66812598e8233
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-12-04 07:54:34 +0100

    %day4r

--- src/main/kotlin/day04r/Day04.kt
+++ src/main/kotlin/day04r/Day04.kt
@@ -4,9 +4,8 @@ package day04r
 
 import java.io.File
 
-val input = File("input/day04.txt")
-
 data class Vec2(val x: Int, val y: Int)
+
 data class Grid(val elems: List<List<Char>>) {
     val allowedDirections = listOf(
         Vec2(1, 0), // east
@@ -19,27 +18,27 @@ data class Grid(val elems: List<List<Char>>) {
         Vec2(1, -1), // northeast
     )
 
+    fun getAtPos(x: Int, y: Int): Char? {
+        return elems.getOrNull(y)?.getOrNull(x)
+    }
+
     fun countXmasWordsForPosition(startX: Int, startY: Int): Int {
-        if (elems[startY][startX] != 'X') {
-            return 0 // invalid word start
+        if (getAtPos(startX, startY) != 'X') {
+            return 0 // invalid word start; also covered by logic of checkXmasWordForDirection
         }
         return allowedDirections.count { direction ->
             checkXmasWordForDirection(startX, startY, direction)
         }
     }
 
-    fun getAtPos(x: Int, y: Int): Char? {
-        return this.elems.getOrNull(y)?.getOrNull(x)
-    }
-
     private fun checkXmasWordForDirection(startX: Int, startY: Int, direction: Vec2): Boolean {
         var runningX = startX
         var runningY = startY
         for (letter in listOf('X', 'M', 'A', 'S')) {
-            if (runningY !in 0..elems.lastIndex || runningX !in 0..elems[0].lastIndex) {
-                return false // out of bounds
+            if (runningY !in elems.indices || runningX !in elems[0].indices) {
+                return false // out of bounds; but also covered by getAtPos returning null
             }
-            if (elems[runningY][runningX] == letter) {
+            if (getAtPos(runningX, runningY) == letter) {
                 runningX += direction.x
                 runningY += direction.y
             } else {
@@ -50,48 +49,50 @@ data class Grid(val elems: List<List<Char>>) {
     }
 
     fun isMASCrossAtPosition(centerX: Int, centerY: Int): Boolean {
-        if (elems[centerY][centerX] != 'A') {
+        if (getAtPos(centerX, centerY) != 'A') {
             return false // invalid cross-center
         }
         // we start at the 'A' of 'MAS', because it is the center.
-        try {
-            val fallingDiagonalOk =
-                elems[centerY - 1][centerX - 1] == 'M' && elems[centerY + 1][centerX + 1] == 'S'
-                        || elems[centerY - 1][centerX - 1] == 'S' && elems[centerY + 1][centerX + 1] == 'M'
+        val isFallingDiagonalMAS =
+            getAtPos(centerX - 1, centerY - 1) == 'M' && getAtPos(centerX + 1, centerY + 1) == 'S'
+        val isFallingDiagonalSAM =
+            getAtPos(centerX - 1, centerY - 1) == 'S' && getAtPos(centerX + 1, centerY + 1) == 'M'
+        val fallingDiagonalOk = isFallingDiagonalMAS || isFallingDiagonalSAM
 
-            val risingDiagonalOk = // copied, careful
-                elems[centerY + 1][centerX - 1] == 'M' && elems[centerY - 1][centerX + 1] == 'S'
-                        || elems[centerY + 1][centerX - 1] == 'S' && elems[centerY - 1][centerX + 1] == 'M'
-            return risingDiagonalOk && fallingDiagonalOk
-        } catch (_: IndexOutOfBoundsException) {
-            return false
-        }
+        val isRisingDiagonalMAS =
+            getAtPos(centerX - 1, centerY + 1) == 'M' && getAtPos(centerX + 1, centerY - 1) == 'S'
+        val isRisingDiagonalSAM =
+            getAtPos(centerX - 1, centerY + 1) == 'S' && getAtPos(centerX + 1, centerY - 1) == 'M'
+        val risingDiagonalOk = isRisingDiagonalMAS || isRisingDiagonalSAM
+
+        return risingDiagonalOk && fallingDiagonalOk
     }
 }
 
 fun main() {
-    val input = input.readLines()
+    val rawInput = File("input/day04.txt")
+    val input = rawInput.readLines()
     val grid = Grid(input.map { it.toCharArray().toList() })
     part1(grid)
     part2(grid)
 }
 
-private fun part2(grid: Grid) {
-    var masSum = 0
-    for (startY in 0..grid.elems[0].lastIndex) {
-        for (startX in 0..grid.elems.lastIndex) {
-            masSum += if (grid.isMASCrossAtPosition(startX, startY)) 1 else 0
+private fun part1(grid: Grid) {
+    var xmasWordCount = 0
+    for (startY in grid.elems[0].indices) {
+        for (startX in grid.elems.indices) {
+            xmasWordCount += grid.countXmasWordsForPosition(startX, startY)
         }
     }
-    println(masSum)
+    println(xmasWordCount)
 }
 
-private fun part1(grid: Grid) {
-    var sum = 0
-    for (startY in 0..grid.elems[0].lastIndex) {
-        for (startX in 0..grid.elems.lastIndex) {
-            sum += grid.countXmasWordsForPosition(startX, startY)
+private fun part2(grid: Grid) {
+    var crossCount = 0
+    for (centerY in grid.elems[0].indices) {
+        for (centerX in grid.elems.indices) {
+            crossCount += if (grid.isMASCrossAtPosition(centerX, centerY)) 1 else 0
         }
     }
-    println(sum)
+    println(crossCount)
 }
