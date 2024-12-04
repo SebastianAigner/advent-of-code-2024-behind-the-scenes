847e1338a6703ed4431c9a44785944266e426350
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-12-04 13:55:58 +0100

    %chr

--- src/main/kotlin/chr/App.kt
+++ src/main/kotlin/chr/App.kt
@@ -1,8 +1,13 @@
 package io.sebi
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -11,12 +16,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.singleWindowApplication
+import day04chr.Grid
 import org.jetbrains.compose.reload.DevelopmentEntryPoint
+import java.io.File
 import java.util.regex.PatternSyntaxException
 
 fun main() {
@@ -29,33 +38,24 @@ fun main() {
     }
 }
 
+val rawInput = File("input/day04.txt")
+val input = rawInput.readLines()
+val grid = Grid(input.map { it.toCharArray().toList() })
+
 @Composable
 fun App() {
-    val enteredRegex = remember { mutableStateOf("") }
-    Column {
-        Text(
-            modifier = Modifier.animateContentSize(),
-            text = "I'm never gonna let you down!",
-            fontSize = 50.sp,
-            color = Color.Magenta,
-        )
-        Button(onClick = {}) {
-            Text("Click me")
-        }
-        TextField(
-            value = enteredRegex.value,
-            onValueChange = { enteredRegex.value = it }
-        )
-        Button(onClick = {
-            val foo = try {
-                enteredRegex.value.toRegex()
-            } catch (e: PatternSyntaxException) {
-                println("not valid!")
-                return@Button
+    Box(Modifier.verticalScroll(rememberScrollState()).semantics(mergeDescendants = true){}) {
+        Box(Modifier.horizontalScroll(rememberScrollState())) {
+            Column(
+            ) {
+                for (y in grid.elems.indices) {
+                    Row() {
+                        for (x in grid.elems[0].indices) {
+                            Text(grid.elems[y][x].toString())
+                        }
+                    }
+                }
             }
-            println("now $foo")
-        }) {
-            Text("Advent me")
         }
     }
 }
---
+++ src/main/kotlin/day04chr/Day04.kt
@@ -0,0 +1,94 @@
+@file:Suppress("DuplicatedCode")
+
+package day04chr
+
+import java.io.File
+
+data class Vec2(val x: Int, val y: Int)
+
+data class Grid(val elems: List<List<Char>>) {
+    val allowedDirections = listOf(
+        Vec2(1, 0), // east
+        Vec2(1, 1), // southeast
+        Vec2(0, 1), // south
+        Vec2(-1, 1), // southwest
+        Vec2(-1, 0), // west
+        Vec2(-1, -1), // northwest
+        Vec2(0, -1), // north
+        Vec2(1, -1), // northeast
+    )
+
+    val indices = sequence {
+        for (y in elems[0].indices) {
+            for (x in elems.indices) {
+                yield(Pair<Int, Int>(x, y))
+            }
+        }
+    }
+
+    fun getAtPos(x: Int, y: Int): Char? = elems.getOrNull(y)?.getOrNull(x)
+
+    fun countXmasWordsForPosition(startX: Int, startY: Int): Int {
+        return allowedDirections.count { direction ->
+            checkXmasWordForDirection(startX, startY, direction)
+        }
+    }
+
+    private fun checkXmasWordForDirection(startX: Int, startY: Int, direction: Vec2): Boolean {
+        var runningX = startX
+        var runningY = startY
+
+        for (letter in listOf('X', 'M', 'A', 'S')) {
+            if (getAtPos(runningX, runningY) != letter) {
+                return false
+            }
+            runningX += direction.x
+            runningY += direction.y
+        }
+        return true
+    }
+
+    fun isMASCrossAtPosition(centerX: Int, centerY: Int): Boolean {
+        if (getAtPos(centerX, centerY) != 'A') {
+            return false // invalid cross-center
+        }
+        // we start at the 'A' of 'MAS', because it is the center.
+        val isFallingDiagonalMAS =
+            getAtPos(centerX - 1, centerY - 1) == 'M' && getAtPos(centerX + 1, centerY + 1) == 'S'
+        val isFallingDiagonalSAM =
+            getAtPos(centerX - 1, centerY - 1) == 'S' && getAtPos(centerX + 1, centerY + 1) == 'M'
+        val fallingDiagonalOk = isFallingDiagonalMAS || isFallingDiagonalSAM
+
+        val isRisingDiagonalMAS =
+            getAtPos(centerX - 1, centerY + 1) == 'M' && getAtPos(centerX + 1, centerY - 1) == 'S'
+        val isRisingDiagonalSAM =
+            getAtPos(centerX - 1, centerY + 1) == 'S' && getAtPos(centerX + 1, centerY - 1) == 'M'
+        val risingDiagonalOk = isRisingDiagonalMAS || isRisingDiagonalSAM
+
+        return risingDiagonalOk && fallingDiagonalOk
+    }
+}
+
+fun main() {
+    val rawInput = File("input/day04.txt")
+    val input = rawInput.readLines()
+    val grid = Grid(input.map { it.toCharArray().toList() })
+    part1(grid)
+    part2(grid)
+}
+
+private fun part1(grid: Grid) {
+    var xmasWordCount = 0
+    for ((startX, startY) in grid.indices) {
+        xmasWordCount += grid.countXmasWordsForPosition(startX, startY)
+    }
+    println(xmasWordCount)
+}
+
+private fun part2(grid: Grid) {
+    var crossCount = 0
+    for ((centerX, centerY) in grid.indices) {
+        crossCount += if (grid.isMASCrossAtPosition(centerX, centerY)) 1 else 0
+    }
+    println(crossCount)
+}
