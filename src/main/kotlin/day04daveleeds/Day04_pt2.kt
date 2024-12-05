95ad70e3f49d1e09a7d3599ef303f22b378a4ec1
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-12-05 06:42:15 +0100

    +daveleeds

--- src/main/kotlin/day04daveleeds/Day04_pt2.kt
+++ src/main/kotlin/day04daveleeds/Day04_pt2.kt
@@ -4,10 +4,8 @@ import daveutils.solve
 import day04.Direction.*
 
 enum class Direction(val x: Int, val y: Int) {
-    NORTH(0, -1), SOUTH(0, 1), EAST(1, 0), WEST(-1, 0), NORTHEAST(1, -1), NORTHWEST(-1, -1), SOUTHEAST(1, 1), SOUTHWEST(
-        -1,
-        1
-    );
+    NORTH(0, -1), SOUTH(0, 1), EAST(1, 0), WEST(-1, 0),
+    NORTHEAST(1, -1), NORTHWEST(-1, -1), SOUTHEAST(1, 1), SOUTHWEST(-1, 1);
 }
 
 operator fun List<String>.get(x: Int, y: Int): Char =
