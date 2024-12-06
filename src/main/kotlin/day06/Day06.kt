d22206924413d99397c9050ed6f30639bf2b1750
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-12-06 06:54:01 +0100

    +day06p2

--- src/main/kotlin/day06/Day06.kt
+++ src/main/kotlin/day06/Day06.kt
@@ -1,6 +1,7 @@
 package day06
 
 import androidx.compose.animation.core.animate
+import java.awt.geom.IllegalPathStateException
 import java.io.File
 
 val input = File("input/day06.txt")
@@ -12,7 +13,7 @@ enum class GuardHeading(val step: Vec2) {
     WEST(Vec2(-1, 0));
 
     fun turnRight(): GuardHeading {
-        return when(this) {
+        return when (this) {
             GuardHeading.NORTH -> EAST
             GuardHeading.EAST -> SOUTH
             GuardHeading.SOUTH -> WEST
@@ -24,49 +25,83 @@ enum class GuardHeading(val step: Vec2) {
 data class Vec2(val x: Int, val y: Int) {
     operator fun plus(other: Vec2) = Vec2(this.x + other.x, this.y + other.y)
 }
+
 data class GuardWalkRecord(val loc: Vec2, val heading: GuardHeading)
+
 fun main() {
     val lines = input.readLines().map { string ->
         string.toList()
     }
+    val visited = simulateGuardPath(lines)
+
+    //p1
+    println(
+        visited
+            .onSuccess { value ->
+                value.distinctBy { record -> record.loc }.size
+            })
+    println("grid size ${lines.indices} x ${lines[0].indices}")
+    //debugPrint(lines, visited)
+    var cnt = 0
+    for (obsY in lines.indices) {
+        for (obsX in lines[0].indices) {
+            // place the obstruction
+            val tile = lines[obsY][obsX]
+            if (tile == '^' || tile == '#') {
+                continue //we're not allowed to place here
+            }
+            val workingCopy = lines.map { chars -> chars.toMutableList() }.toMutableList()
+            workingCopy[obsY][obsX] = '#' // no need for 'O'.
+            val res = simulateGuardPath(workingCopy)
+            if (res.isFailure) {
+                cnt++
+            }
+        }
+    }
+    println(cnt)
+}
+
+private fun simulateGuardPath(lines: List<List<Char>>): Result<List<GuardWalkRecord>> {
     var guardPos = Vec2(-1, -1)
     var guardHeading = GuardHeading.NORTH
-    val visited = mutableListOf<Vec2>()
+    val visited = mutableListOf<GuardWalkRecord>()
     for (y in lines.indices) {
         for (x in lines[0].indices) {
             if (lines[y][x] == '^') {
                 guardPos = Vec2(x, y)
-                visited += Vec2(x, y)
+                visited += GuardWalkRecord(guardPos, guardHeading)
             }
         }
     }
     check(guardPos.y != -1 && guardPos.x != -1)
     fun getTile(x: Int, y: Int): Char {
-        return if(y !in lines.indices || x !in lines[0].indices) '?' else lines[y][x]
+        return if (y !in lines.indices || x !in lines[0].indices) '?' else lines[y][x]
     }
-    println(guardPos)
     while (guardPos.y in lines.indices && guardPos.x in lines[0].indices) {
         // take a step
         val attemptedNewPos = guardPos + guardHeading.step
         val targetTile = getTile(attemptedNewPos.x, attemptedNewPos.y)
         if (targetTile == '.' || targetTile == '^') {
             guardPos = attemptedNewPos
-            println("took a step to $guardPos")
-            visited += attemptedNewPos
-        } else if(targetTile == '#') {
-            println("hit a thing")
+            //println("took a step to $guardPos")
+            val record = GuardWalkRecord(attemptedNewPos, guardHeading)
+            if (record in visited) {
+               println("this feels familiar.")
+                return Result.failure(IllegalPathStateException())
+            }
+            visited += record
+        } else if (targetTile == '#') {
+           // println("hit a thing")
             guardHeading = guardHeading.turnRight()
-        } else if(targetTile == '?') {
-            println("we're out of bounds at $attemptedNewPos")
+        } else if (targetTile == '?') {
+           // println("we're out of bounds at $attemptedNewPos")
             break
         } else {
-            println("i've never seen a $targetTile before")
+            //println("i've never seen a $targetTile before")
         }
         //debugPrint(lines,visited)
     }
-    println(visited.distinct().size)
-    println("grid size ${lines.indices} x ${lines[0].indices}")
-    //debugPrint(lines, visited)
+    return Result.success(visited)
 }
 
 private fun debugPrint(
