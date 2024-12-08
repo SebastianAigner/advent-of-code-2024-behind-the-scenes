63fbb50a7a04623ce0d8cac2094c0cabf328ae45
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-12-08 06:57:07 +0100

    +day08p2

--- src/main/kotlin/day08/Day08.kt
+++ src/main/kotlin/day08/Day08.kt
@@ -12,57 +12,96 @@ data class Vec2(val x: Int, val y: Int) {
 
 fun main() {
     val lines = input.readLines()
-    fun getChr(x: Int, y: Int) = lines[y][x]
     val mapYs = lines.indices
     val mapXs = lines[0].indices
-    val positionToType = mutableMapOf<Vec2, Char>()
+    val typeToPosition = createLookup(lines)
+    val antiNodeLocations1 = part1(
+        typeToPosition = typeToPosition
+    ) {
+        x in mapXs && y in mapYs
+    }
+    val antiNodeLocations2 = part2(
+        typeToPosition = typeToPosition
+    ) {
+        x in mapXs && y in mapYs
+    }
+    println(antiNodeLocations1.size)
+    println(antiNodeLocations2.size)
+}
+
+fun List<String>.getChr(x: Int, y: Int) = this[y][x]
+
+private fun createLookup(lines: List<String>): Map<Char, MutableSet<Vec2>> {
     val typeToPosition = mutableMapOf<Char, MutableSet<Vec2>>()
-    // do an inverse index?
     for (y in lines.indices) {
         for (x in lines[0].indices) {
-            val chr = getChr(x, y)
+            val chr = lines.getChr(x, y)
             if (chr == '.') {
                 continue
             }
-            positionToType += Vec2(x, y) to chr
             val ttpEntry = typeToPosition.getOrElse(chr) { mutableSetOf() }
             ttpEntry.add(Vec2(x, y))
             typeToPosition.put(chr, ttpEntry)
         }
     }
+    return typeToPosition
+}
+
+
+fun part1(
+    typeToPosition: Map<Char, MutableSet<Vec2>>,
+    isInBounds: Vec2.() -> Boolean,
+): MutableSet<Vec2> {
     val antiNodeLocations = mutableSetOf<Vec2>()
-    fun Vec2.isInMap(): Boolean = x in mapXs && y in mapYs
     for (type in typeToPosition.keys) {
-        println(type)
         val kin = typeToPosition[type]!!
-        println(kin)
         for (first in kin) {
             for (second in kin) {
                 if (first == second) continue
-                // compute antinode locations
-                // distance between
+                // compute vector between antinode locations
+                val distanceVec = second - first
+                val relDistVec = distanceVec * 2
+                val absAntiNodeVec = first + relDistVec
+                if (absAntiNodeVec.isInBounds()) {
+                    // MAKE SURE YOU ONLY COUNT wHATS IN THE MAP
+                    antiNodeLocations += absAntiNodeVec
+                }
+            }
+        }
+    }
+    return antiNodeLocations
+}
+
+private fun part2(
+    typeToPosition: Map<Char, MutableSet<Vec2>>,
+    isInBounds: Vec2.() -> Boolean,
+): MutableSet<Vec2> {
+    val antiNodeLocations = mutableSetOf<Vec2>()
+    for (type in typeToPosition.keys) {
+        val kin = typeToPosition[type]!!
+        for (first in kin) {
+            for (second in kin) {
+                if (first == second) continue
                 val distanceVec = second - first
                 val relDistVec = distanceVec
                 var absAntiNodeVec: Vec2 = first
                 do {
                     absAntiNodeVec = absAntiNodeVec + relDistVec
-                    if (absAntiNodeVec.isInMap()) {
+                    if (absAntiNodeVec.isInBounds()) {
                         // MAKE SURE YOU ONLY COUNT wHATS IN THE MAP
                         antiNodeLocations += absAntiNodeVec
                     }
-                } while (absAntiNodeVec.isInMap())
+                } while (absAntiNodeVec.isInBounds())
                 var absAntiNodeVec2: Vec2 = first
                 do {
                     absAntiNodeVec2 = absAntiNodeVec2 - relDistVec
-                    if (absAntiNodeVec2.isInMap()) {
+                    if (absAntiNodeVec2.isInBounds()) {
                         // MAKE SURE YOU ONLY COUNT wHATS IN THE MAP
                         antiNodeLocations += absAntiNodeVec2
                     }
-                } while (absAntiNodeVec.isInMap())
+                } while (absAntiNodeVec.isInBounds())
             }
         }
     }
-    println(antiNodeLocations.size)
+    return antiNodeLocations
 }
-
-// MAKE SURE YOU ONLY COUNT wHATS IN THE MAP
