8e38f8ddaadc21655423b8b542582a989634fd54
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-12-10 06:52:17 +0100

    %day10p2

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -11,21 +11,6 @@ data class Vec2(val x: Int, val y: Int) {
     fun right() = Vec2(x + 1, y)
 }
 
-fun main() {
-    val lines = input.readLines()
-    val map = buildMapFromLines(lines)
-    val potentialTrailheads = map.filterValues { it == 0 }
-    val a = potentialTrailheads.toList().sumOf { (pth, _) ->
-        countReachableSummitsForTrailhead(pth, map)
-    }.also(::println)
-
-    val b = potentialTrailheads.toList().sumOf { (pth, _) ->
-        countDistinctPathsToSummitForTrailhead(pth, map)
-    }.also(::println)
-    check(a == 510)
-    check(b == 1058)
-}
-
 private fun buildMapFromLines(lines: List<String>): Map<Vec2, Int> = buildMap<Vec2, Int> {
     for (y in lines.indices) {
         for (x in lines[0].indices) {
@@ -35,12 +20,48 @@ private fun buildMapFromLines(lines: List<String>): Map<Vec2, Int> = buildMap<Ve
     }
 }
 
+fun main() {
+    val lines = input.readLines()
+    val map = buildMapFromLines(lines)
+    val potentialTrailheads = map.filterValues { it == 0 }.toList()
+    val a = potentialTrailheads.sumOf { (pth, _) ->
+        countReachableSummitsForTrailhead(pth, map)
+    }.also(::println)
+
+    val b = potentialTrailheads.sumOf { (pth, _) ->
+        countDistinctPathsToSummitForTrailhead(pth, map)
+    }.also(::println)
+    check(a == 510)
+    check(b == 1058)
+}
+
+fun Map<Vec2, Int>.getCoord(v: Vec2) = this[v] ?: Int.MAX_VALUE // a _very_ steep cliff.
+
 fun countReachableSummitsForTrailhead(trailhead: Vec2, map: Map<Vec2, Int>): Int {
     require(map[trailhead] == 0)
     val reachableSummits = walkPathsToSummits(listOf(trailhead), map)
     check(reachableSummits.all { summit -> map.getCoord(summit.location) == 9 })
     return reachableSummits.toSet().size
 }
+
+@JvmInline
+value class Summit(val location: Vec2)
+
+fun walkPathsToSummits(path: List<Vec2>, map: Map<Vec2, Int>): List<Summit> {
+    val curLoc = path.last()
+    val curLevel = map.getCoord(curLoc)
+    if (curLevel == 9) return listOf(Summit(curLoc))
+    val nextLevel = curLevel + 1
+    val reachableSummits = with(curLoc) {
+        listOf(up(), down(), left(), right())
+    }.filter {
+        map.getCoord(it) == nextLevel
+    }.flatMap {
+        walkPathsToSummits(path + it, map)
+    }
+    return reachableSummits
+}
+
 
 fun countDistinctPathsToSummitForTrailhead(trailhead: Vec2, map: Map<Vec2, Int>): Int {
     require(map[trailhead] == 0)
@@ -57,32 +78,12 @@ fun tracePathsToSummits(path: List<Vec2>, map: Map<Vec2, Int>): List<Path> {
     val curLevel = map.getCoord(curLoc)
     if (curLevel == 9) return listOf(Path(path))
     val nextLevel = curLevel + 1
-    val walkablePaths = mutableListOf<Path>()
-    for (neighbor in with(curLoc) { listOf(up(), down(), left(), right()) }) {
-        if (map.getCoord(neighbor) == nextLevel) {
-            val potentialTargets = tracePathsToSummits(path + neighbor, map)
-            walkablePaths += potentialTargets
-        }
+    val walkablePaths = with(curLoc) {
+        listOf(up(), down(), left(), right())
+    }.filter {
+        map.getCoord(it) == nextLevel
+    }.flatMap {
+        tracePathsToSummits(path + it, map)
     }
     return walkablePaths
-}
-
-fun Map<Vec2, Int>.getCoord(v: Vec2) = this[v] ?: Int.MAX_VALUE // a _very_ steep cliff.
-
-@JvmInline
-value class Summit(val location: Vec2)
-
-fun walkPathsToSummits(path: List<Vec2>, map: Map<Vec2, Int>): List<Summit> {
-    val curLoc = path.last()
-    val curLevel = map.getCoord(curLoc)
-    if (curLevel == 9) return listOf(Summit(curLoc))
-    val nextLevel = curLevel + 1
-    val reachableSummits = mutableListOf<Summit>()
-    for (neighbor in with(curLoc) { listOf(up(), down(), left(), right()) }) {
-        if (map.getCoord(neighbor) == nextLevel) {
-            val foundSummits = walkPathsToSummits(path + neighbor, map)
-            reachableSummits += foundSummits
-        }
-    }
-    return reachableSummits
 }
