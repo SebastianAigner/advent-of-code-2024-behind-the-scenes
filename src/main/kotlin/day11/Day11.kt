d974440e6fec4333c7c0c0e6de1436a20977855a
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-12-11 06:13:57 +0100

    +day11p1

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -1,10 +1,37 @@
 package day11
 
 import java.io.File
+import java.math.BigInteger
 
 val input = File("input/day11.txt")
 
 fun main() {
-    val lines = input.readLines()
-    println(lines)
+    var line = input.readLines().first().split(" ").map { string -> string.toBigInteger() }
+    repeat(75) {
+        println(it)
+        line = blink(line)
+    }
+    println(line.size)
+}
+
+fun blink(integers: List<BigInteger>): List<BigInteger> {
+    return buildList<BigInteger> {
+        for (i in integers) {
+            when {
+                i == BigInteger.ZERO -> {
+                    add(BigInteger.ONE)
+                }
+
+                i.toString().length % 2 == 0 -> { // even digits
+                    val len = i.toString().length
+                    add(i.toString().take(len / 2).toBigInteger())
+                    add(i.toString().takeLast(len / 2).toBigInteger())
+                }
+
+                else -> {
+                    add(i * BigInteger.valueOf(2024))
+                }
+            }
+        }
+    }
 }
