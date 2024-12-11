6df5802bf8e3ad6ce60e3c83519c3cbd5c98a333
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-12-11 06:41:03 +0100

    +day11p2

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -6,32 +6,66 @@ import java.math.BigInteger
 val input = File("input/day11.txt")
 
 fun main() {
-    var line = input.readLines().first().split(" ").map { string -> string.toBigInteger() }
-    repeat(75) {
-        println(it)
-        line = blink(line)
+    val line = input.readLines().first().split(" ").map { string -> string.toBigInteger() }
+    var groups = line.groupingBy { integer -> integer }.eachCount()
+    var nmList = NonMaterializedList()
+    for ((k, v) in groups) {
+        nmList.add(k, BigInteger.valueOf(v.toLong()))
     }
-    println(line.size)
+    repeat(75) {
+        nmList = blinkGroups(nmList)
+        println(nmList)
+    }
+    println(nmList.coll.values.sumOf { integer -> integer.toLong() })
 }
 
-fun blink(integers: List<BigInteger>): List<BigInteger> {
-    return buildList<BigInteger> {
-        for (i in integers) {
+fun blinkGroups(nmList: NonMaterializedList): NonMaterializedList {
+    return NonMaterializedList().apply {
+        for ((key, value) in nmList.coll) {
             when {
-                i == BigInteger.ZERO -> {
-                    add(BigInteger.ONE)
+                key == BigInteger.ZERO -> {
+                    add(BigInteger.ONE, value)
                 }
 
-                i.toString().length % 2 == 0 -> { // even digits
-                    val len = i.toString().length
-                    add(i.toString().take(len / 2).toBigInteger())
-                    add(i.toString().takeLast(len / 2).toBigInteger())
+                key.toString().length % 2 == 0 -> {
+                    val len = key.toString().length
+                    val top = key.toString().take(len / 2).toBigInteger()
+                    val bot = key.toString().takeLast(len / 2).toBigInteger()
+                    add(top, value)
+                    add(bot, value)
                 }
 
                 else -> {
-                    add(i * BigInteger.valueOf(2024))
+                    add(key * BigInteger.valueOf(2024), value)
                 }
             }
         }
+    }
+}
+
+fun blinkOne(i: BigInteger): List<BigInteger> {
+    return when {
+        i == BigInteger.ZERO -> {
+            listOf(BigInteger.ONE)
+        }
+
+        i.toString().length % 2 == 0 -> { // even digits
+            val len = i.toString().length
+            listOf(
+                i.toString().take(len / 2).toBigInteger(),
+                i.toString().takeLast(len / 2).toBigInteger()
+            )
+        }
+
+        else -> {
+            listOf(i * BigInteger.valueOf(2024))
+        }
+    }
+}
+
+data class NonMaterializedList(val coll: MutableMap<BigInteger, BigInteger> = mutableMapOf()) {
+    fun add(b: BigInteger, count: BigInteger) {
+        val curr = coll[b] ?: BigInteger.ZERO
+        coll[b] = curr + count
     }
 }
