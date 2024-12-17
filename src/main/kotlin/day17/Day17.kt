4bff1f0d333adbda56f5ef7182e7eed74373a210
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-12-17 06:48:01 +0100

    +day17p1broken

---
+++ src/main/kotlin/day17/Day17.kt
@@ -0,0 +1,96 @@
+package day17
+
+import java.io.File
+
+val input = File("input/day17.txt")
+
+fun main() {
+    val lines = input.readLines()
+    var regA = lines[0].takeLastWhile { it.isDigit() }.toInt()
+    var regB = lines[1].takeLastWhile { it.isDigit() }.toInt()
+    var regC = lines[2].takeLastWhile { it.isDigit() }.toInt()
+    var ip = -1
+    val program = lines[4].removePrefix("Program: ").split(",").map { it.toInt() }
+    fun valueOfComboOp(op: Int): Int {
+        return when (op) {
+            in 0..3 -> {
+                op
+            }
+
+            4 -> regA
+            5 -> regB
+            6 -> regC
+            7 -> error("not a valid program")
+            else -> error("not valid either!")
+        }
+    }
+
+    val final = StringBuilder()
+    do {
+        val inst = program[++ip]
+        println("INST $inst at IP $ip")
+        when (inst) {
+            0 -> {
+                // ADV num/denom
+                val num = regA
+                val denom = valueOfComboOp(program[++ip])
+                val res = num / denom
+                regA = res
+            }
+
+            1 -> {
+                // BXL
+                val x = regA
+                val lit = program[++ip]
+                regB = x xor lit
+            }
+
+            2 -> {
+                // BST
+                val co = valueOfComboOp(program[++ip])
+                val modded = co % 8
+                regB = modded
+            }
+
+            3 -> {
+                // JNZ
+                if (regA == 0) {
+                    ip += 2
+                } else {
+                    ip = program[ip + 1]
+                }
+            }
+
+            4 -> {
+                // BXC
+                val new = regB xor regC
+                regB = new
+                ip += 2
+            }
+
+            5 -> {
+                // OUT
+                val output = valueOfComboOp(program[++ip]) % 8
+                println("OUT $output")
+                final.append("$output,")
+            }
+
+            6 -> {
+                // BDV
+                val num = regA
+                val denom = valueOfComboOp(program[++ip])
+                val res = num / denom
+                regB = res
+            }
+
+            7 -> {
+                val num = regA
+                val denom = valueOfComboOp(program[++ip])
+                val res = num / denom
+                regC = res
+            }
+        }
+    } while (ip + 1 in program.indices)
+    println("A $regA  B $regB  C $regC  D $ip")
+    println(final)
+}
